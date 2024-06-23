7c12ce8dfc27b49bfba8bad791b6408092788bc9
Marvin Saße <marvinleander@googlemail.com>
2024-06-23 14:59:32 +0200

    feat(module): add module export

--- src/pokemon/index.ts
+++ src/pokemon/index.ts
@@ -1,7 +1,7 @@
 import { CardList, SupportedLanguages } from "@tcgdex/sdk";
 import HttpService from "../utils/axios.util";
 
-export default function getCardsByLanguage(
+export function getCardsByLanguage(
   language: SupportedLanguages
 ): Promise<CardList> {
   let httpService = new HttpService();
