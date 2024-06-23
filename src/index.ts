088c8b61af66f29d923ba67a0c2c4fe27618db4e
Marvin Saße <marvinleander@googlemail.com>
2024-06-23 14:16:18 +0200

    fix(npm): change back to npm

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,1 @@
-/**
- * @description
- * @param {number} n
- * @returns {boolean}
- * @example
- * isOdd(1) // true
- * isOdd(2) // false
- * */
-export const isOdd = (n: number) => n % 2 === 1;
+export * from "./pokemon/index";
